caaff7e78217074c00f5ee4bafa37f7b3892a783
Fredrik Kujansuu <fredrik.k91@gmail.com>
2023-09-07 01:59:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-wom_group_id = # ID for clan must be int
-discord_webhook = "" # webhook URL for discord must be String
+wom_group_id = # ID for clan, must be int
+wom_verification_code = "" # Verification code for WoM group, must be String
+discord_webhook = "" # webhook URL for discord, must be String
