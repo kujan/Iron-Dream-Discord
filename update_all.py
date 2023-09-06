19ee4a687ade6b0e511765f176c50d40683d262f
Fredrik Kujansuu <fredrik.k91@gmail.com>
2023-09-07 01:59:49 +0200

    Update update_all.py

--- update_all.py
+++ update_all.py
@@ -3,6 +3,6 @@ import requests
 
 code = config.wom_verification_code
 group_id = config.wom_group_id
-myobj = {"verificationCode": code}
-r = requests.post(f"https://api.wiseoldman.net/v2/groups/{group_id}/update-all", data = myobj)
+data = {"verificationCode": code}
+r = requests.post(f"https://api.wiseoldman.net/v2/groups/{group_id}/update-all", data = data)
 print(r.text)
