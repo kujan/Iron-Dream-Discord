0ba9af7e91525fc02a38cd0c3c916bf811c2c95f
Fredrik Kujansuu <fredrik.k91@gmail.com>
2023-09-16 15:35:10 +0200

    update top_weekly

--- config.py
+++
@@ -1,3 +0,0 @@
-wom_group_id = # ID for clan, must be int
-wom_verification_code = "" # Verification code for WoM group, must be String
-discord_webhook = "" # webhook URL for discord, must be String
